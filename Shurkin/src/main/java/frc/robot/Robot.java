fb25cfb22dcd13afc0d219d7417ff19e977a230d
IAN <73033682+IAN123456789012345678901234567890@users.noreply.github.com>
2021-09-25 10:33:45 -0700

    Update Robot.java

--- Shurkin/src/main/java/frc/robot/Robot.java
+++ Shurkin/src/main/java/frc/robot/Robot.java
@@ -4,95 +4,111 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+
+
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  // drive
+  WPI_TalonFX RightDriveTalon;
+  WPI_TalonFX LeftDriveTalon;
+  // shooter
+  WPI_TalonFX LeftShooter;
+  WPI_TalonFX RightShooter;
+  //funnels
+  WPI_TalonFX FunnelLeft;
+  WPI_TalonFX FunnelRight;
+  // controller
+  XboxController controller;
+
+
+
+  // doubles & booleans
+  double rightSide;
+  double leftSide;
+  boolean buttonAIsPressed = false;
+  
+
+
+  
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    
+    RightDriveTalon = new WPI_TalonFX(1);
+    LeftDriveTalon = new WPI_TalonFX(2);
+
+    LeftShooter = new WPI_Talon(3);
+    RightShooter = new WPI_Talon(4);
+    
+    FunnelLeft = new WPI_TalonFX(5);
+    FunnelRight = new WPI_TalonFX(6);
+
+    controller  = new XboxController(0);
+    
+
+
+
+    
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    
   }
 
-  /** This function is called periodically during autonomous. */
+  
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    
     }
-  }
+  
 
-  /** This function is called once when teleop is enabled. */
+  
   @Override
   public void teleopInit() {}
-
-  /** This function is called periodically during operator control. */
+  
+  
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    if (m_controller.getTriggerAxis(Hand.kLeft) > 0.1) {
+      LeftShooter.set(ControlMode.PercentOutput, 0.7);
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+
+
+
+
+
+    // drive train
+    rightSide = (-controller.getRawAxis(4) + controller.getRawAxis(1));
+    leftSide = (-controller.getRawAxis(4) - controller.getRawAxis(1));
+
+    // Setting motor with double
+    RightDriveTalon.set(ControlMode.PercentOutput, -rightSide);
+    LeftDriveTalon.set(ControlMode.PercentOutput, -leftSide);
+
+  }
+  
+
+  
+
+
+
+
+  }
+
 }
+
